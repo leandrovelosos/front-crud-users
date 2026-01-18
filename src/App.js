609843aa10803649a3d516af2a0c799f6bd79dfe
leandrovelosos <leandrovdsantos96@gmail.com>
2026-01-18 12:40:07 -0300

    formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import GlobalStyle from './styles/global';
+import styled from 'styled-components';
+import Form from './components/Form';
+import { Toaster, toast } from 'react-hot-toast';
+//import "react-toastify/dist/ReactToastify.css";
 
-import './App.css';
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px
+`
+const Title = styled.h2``;
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Container>
+        <Title>Usuários</Title>
+        <Form />
+      </Container>
+      
+      <Toaster position="bottom-left" />
+      <GlobalStyle />
+    </>
   );
 }
 
